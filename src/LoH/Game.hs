a1fd8e43eb41a6ad3bfdbe766779836678aeedb6
Kris Nuttycombe <kris.nuttycombe@gmail.com>
2018-12-07 12:14:17 -0700

    Implement initial skeleton of the land-of-haskell games.

--- src/LoH/Game.hs
+++ src/LoH/Game.hs
@@ -8,53 +8,68 @@ newtype Location where
   deriving (Eq, Ord)
 
 livingRoom :: Location
-livingRoom = undefined
+livingRoom = ToLoc { locName = "living-room" }
 
-garden :: Location
+garden = ToLoc { locName = "garden" }
 garden = undefined
 
-attic :: Location
+attic = ToLoc { locName = "attic" }
 attic = undefined
 
--- newtype Description where
---   ToDesc :: { descText :: Text } -> Description
---
--- type Locations = M.Map Location Description
---
--- locations :: Locations
--- locations = undefined
---
--- describeLocation :: Locations -> Location -> Maybe Description
--- describeLocation = undefined
---
--- data Path where
---   ToPath :: { dest :: Location, dir :: Text, via :: Text } -> Path
---
--- type Paths = M.Map Location [Path]
---
--- paths :: Paths
--- paths = undefined
---
--- describePath :: Path -> Text
--- describePath = undefined
---
--- describePathsFrom :: Paths -> Location -> [Text]
--- describePathsFrom = undefined
---
--- newtype Item where
---   ToItem :: { itemName :: Text } -> Item
---   deriving (Eq, Ord)
---
--- type Items = M.Map Location [Item]
---
--- items :: Items
--- items = undefined
---
--- data Environment where
---   Env :: { currentLocation :: Location
---          , envLocations :: Locations
---          , envPaths :: Paths
---          , envItems :: Items
---          } -> Environment
+newtype Description where
+  ToDesc :: { descText :: Text } -> Description
 
+type Locations = M.Map Location Description
 
+locations :: Locations
+locations =
+  M.fromList [ (livingRoom, ToDesc "You are in the living-room. A wizard is snoring loudly on the couch.")
+             , (garden, ToDesc "You are in a beautiful garden. There is a well in front of you.")
+             , (attic, ToDesc "You are in the attic. There is a giant welding torch in the corner.")
+             ]
+
+describeLocation :: Locations -> Location -> Maybe Description
+describeLocation locs loc = M.lookup loc locs
+
+data Path where
+  ToPath :: { dest :: Location, dir :: Text, via :: Text } -> Path
+
+type Paths = M.Map Location [Path]
+
+paths :: Paths
+paths =
+  M.fromList [ (livingRoom, [ ToPath { dest = garden, dir = "west", via = "door" }
+                            , ToPath { dest = attic,  dir = "upstairs", via = "ladder" }])
+             , (garden,     [ ToPath { dest = livingRoom, dir = "east", via = "door" }])
+             , (attic,      [ ToPath { dest = livingRoom, dir = "downstairs", via = "ladder" }])
+             ]
+
+describePath :: Path -> Text
+describePath p =
+  concat ["There is a ", via p," going ", dir p, " from here"]
+
+describePathsFrom :: Paths -> Location -> [Text]
+describePathsFrom ps loc =
+  maybe
+    ["I can't find that location!"]
+    (\locPaths -> mapList describePath locPaths)
+    (M.lookup loc ps)
+
+newtype Item where
+  ToItem :: { itemName :: Text } -> Item
+  deriving (Eq, Ord)
+
+type Items = M.Map Location [Item]
+
+items :: Items
+items =
+  M.fromList [ (garden, mapList ToItem ["frog", "chain"])
+             , (livingRoom, mapList ToItem ["whiskey", "bucket"])
+             ]
+
+data Environment where
+  Env :: { currentLocation :: Location
+         , envLocations :: Locations
+         , envPaths :: Paths
+         , envItems :: Items
+         } -> Environment
--- src/LoH/GuessMe.hs
+++ src/LoH/GuessMe.hs
@@ -3,13 +3,29 @@ module LoH.GuessMe where
 import Introlude
 
 guess :: Int -> Int -> Int
-guess = undefined
+guess small big = divInt (addInt small big) 2
 
 printGuess :: Int -> Action ()
-printGuess = undefined
+printGuess i =
+  putTextAction (concat ["My guess is ", renderInt i])
 
 guessMyNumber :: Int -> Int -> Action ()
-guessMyNumber = undefined
+guessMyNumber small big =
+  let latestGuess = guess small big
+
+      doCommand :: Text -> Action ()
+      doCommand command = case command of
+        "smaller" -> guessMyNumber small latestGuess
+        "bigger"  -> guessMyNumber latestGuess big
+        "quit"    -> putTextAction "It was fun playing, goodbye!"
+        other     ->
+          (putTextAction (concat ["I didn't understand \"", other, "\", can you try again?"]))
+          `andThen`
+          (guessMyNumber small big)
+
+   in (printGuess latestGuess)
+      `andThen`
+      (withActionResult getTextAction doCommand)
 
 startGame =
   runAction (
